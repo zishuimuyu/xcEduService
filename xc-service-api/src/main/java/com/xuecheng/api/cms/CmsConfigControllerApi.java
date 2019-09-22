b60248792af25c0cff594fae6aad55003efd5030
zishuimuyu <790491153@qq.com>
2019-09-23 01:04:31 +0800

    开发页面静态化

---
+++ xc-service-api/src/main/java/com/xuecheng/api/cms/CmsConfigControllerApi.java
@@ -0,0 +1,23 @@
+package com.xuecheng.api.cms;
+
+import com.xuecheng.framework.domain.cms.CmsConfig;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
+/**
+ * TODO
+ *
+ * @author：GJH
+ * @createDate：2019/9/22
+ * @company：洪荒宇宙加力蹲大学
+ */
+@Api(value = "cms配置管理接口",description = "cms配置管理接口,提供数据类型管理/查询接口")
+public interface CmsConfigControllerApi {
+    /**
+     * 根据id查询cms配置信息
+     * @param id cms配置信息ID
+     * @return
+     */
+    @ApiOperation("根据id查询cms配置信息")
+    public CmsConfig getModel(String id);
+}
---
+++ xc-service-manage-cms/src/main/java/com/xuecheng/manage_cms/dao/CmsConfigRepository.java
@@ -0,0 +1,25 @@
+package com.xuecheng.manage_cms.dao;
+
+
+import com.xuecheng.framework.domain.cms.CmsConfig;
+import com.xuecheng.framework.domain.cms.CmsPage;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface CmsConfigRepository extends MongoRepository<CmsConfig,String> {
+    /**
+     * 根据页面名称查询
+     * @param pageName
+     * @return
+     */
+   // CmsPage findByPageName(String pageName);
+
+    /**
+     * 根据页面名称,站点ID,页面路径查找页面
+     * @param pageName
+     * @param siteId
+     * @param pageWebPath
+     * @return
+     */
+  //  CmsPage findByPageNameAndSiteIdAndPageWebPath(String pageName, String siteId, String pageWebPath);
+
+}
