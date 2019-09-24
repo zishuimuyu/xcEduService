ef456b85c4293bac8397f3e6654d8636ceb818ba
zishuimuyu <790491153@qq.com>
2019-09-25 00:14:34 +0800

    更正自定义异常抛出类中的错误

--- xc-framework-common/src/main/java/com/xuecheng/framework/exception/ExceptionCatch.java
+++ xc-framework-common/src/main/java/com/xuecheng/framework/exception/ExceptionCatch.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
- * 自定义异常捕获类
+ * 自定义统一异常捕获类
  *
  * @author：GJH
  * @createDate：2019/9/19
@@ -52,7 +52,7 @@ public class ExceptionCatch {
      * 设定这个自定义捕获类要捕获CustomException 异常
      */
     @ResponseBody
-    @ExceptionHandler(CustomException.class)
+    @ExceptionHandler(Exception.class)
     public ResponseResult exception(Exception exception) {
         //记录日志
         LOGGER.error("捕获异常:", exception.getMessage());
