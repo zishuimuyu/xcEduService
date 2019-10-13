0848dbfcf792a92fff80de33970d706930372373
zishuimuyu <790491153@qq.com>
2019-10-13 22:52:29 +0800

    把课程管理和cms管理模块加入Rureka注册中心

--- xc-service-manage-course/src/main/java/com/xuecheng/manage_course/ManageCourseApplication.java
+++ xc-service-manage-course/src/main/java/com/xuecheng/manage_course/ManageCourseApplication.java
@@ -3,6 +3,7 @@ package com.xuecheng.manage_course;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
 /**
@@ -11,6 +12,7 @@ import org.springframework.context.annotation.ComponentScan;
  * @createDate：2019/9/28
  * @company：洪荒宇宙加力蹲大学
  */
+@EnableDiscoveryClient //表明这是EurekaClient从EurekaServer中发现服务
 @SpringBootApplication
 @EntityScan("com.xuecheng.framework.domain.course")//扫描实体类
 @ComponentScan(basePackages={"com.xuecheng.api"})//扫描接口
