4df17f620ee7ef89c3d4df5cb169a2242819ecda
790491153@qq.com <guojunhua1991>
2019-10-14 09:41:13 +0800

    测试文件读取功能,调整pom文件

--- xc-framework-utils/src/main/java/com/xuecheng/framework/test.java
+++ xc-framework-utils/src/main/java/com/xuecheng/framework/test.java
@@ -2,7 +2,9 @@ package com.xuecheng.framework;
 
 import cn.hutool.core.io.FileUtil;
 import net.lingala.zip4j.core.ZipFile;
+import org.apache.commons.io.FileUtils;
 import org.apache.commons.io.FilenameUtils;
+import org.apache.commons.io.IOUtils;
 
 import java.io.File;
 
@@ -14,22 +16,24 @@ public class test {
     public static void main(String[] args) throws Exception {
         String path = "F:\\项目\\15-学成在线\\day08 课程图片管理 分布式文件系统\\代码\\xcEduService01";
         File file = new File(path);
-        File[] files = file.listFiles();
-
-
-        for (File file1 : files) {
-            File[] files1 = file1.listFiles();
-            for (File file2 : files1) {
-                System.out.println(file2.getName());
-                if (file2.getName().endsWith(".iml") || file2.getName() == ".gitignore") {
-                    file2.delete();
+        if(file.isDirectory()){
+            File[] files = file.listFiles();
+            for (File file1 : files) {
+                if (file1.getName().equals(".idea")){
+                    FileUtil.del(file1);
                 }
-                if (file2.getName() == "target") {
-                    FileUtil.del(file2);
-
+                if (file1.exists()){
+                    File[] files1 = file1.listFiles();
+                    for (File file2 : files1) {
+                        String name = file2.getName();
+                        if (name.equals("target")||name.endsWith(".iml")||name.equals(".idea")||name.equals(".gitignore")){
+                            FileUtil.del(file2);
+                        }
+                    }
                 }
+
+
             }
-
         }
 
     }
