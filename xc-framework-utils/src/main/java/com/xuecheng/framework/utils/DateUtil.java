13abf89d7a8318f9904216b93b9f41a2e73bffd5
zishuimuyu <790491153@qq.com>
2019-10-31 21:39:52 +0800

    DateUtil新增获取指定月份第一天和最后一天日期的方法

--- xc-framework-utils/src/main/java/com/xuecheng/framework/utils/DateUtil.java
+++ xc-framework-utils/src/main/java/com/xuecheng/framework/utils/DateUtil.java
@@ -998,4 +998,49 @@ public final class DateUtil {
         SimpleDateFormat dates = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         return dates.format(nextTime);
     }
+    /**
+     * 获得该月第一天
+     * @param year
+     * @param month
+     * @return
+     */
+    public static String getFirstDayOfMonth(int year,int month){
+        Calendar cal = Calendar.getInstance();
+        cal.clear();
+        //设置年份
+        cal.set(Calendar.YEAR,year);
+        //设置月份
+        cal.set(Calendar.MONTH, month-1);
+        //获取某月最小天数
+        int firstDay = cal.getActualMinimum(Calendar.DAY_OF_MONTH);
+        //设置日历中月份的最小天数
+        cal.set(Calendar.DAY_OF_MONTH, firstDay);
+        //格式化日期
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String firstDayOfMonth = sdf.format(cal.getTime());
+        return firstDayOfMonth;
+    }
+
+    /**
+     * 获得该月最后一天
+     * @param year
+     * @param month
+     * @return
+     */
+    public static String getLastDayOfMonth(int year,int month){
+        Calendar cal = Calendar.getInstance();
+        cal.clear();
+        //设置年份
+        cal.set(Calendar.YEAR,year);
+        //设置月份
+        cal.set(Calendar.MONTH, month-1);
+        //获取某月最大天数
+        int lastDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+        //设置日历中月份的最大天数
+        cal.set(Calendar.DAY_OF_MONTH, lastDay);
+        //格式化日期
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        String lastDayOfMonth = sdf.format(cal.getTime());
+        return lastDayOfMonth;
+    }
 }
